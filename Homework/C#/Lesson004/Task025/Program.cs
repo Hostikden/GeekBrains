4a78e579020766954a7d305623df2521a20a3a38
Hostik <Hostik.den@gmail.com>
2023-01-14 18:37:23 +0400

    add Lesson004Task025

---
+++ Homework/C#/Lesson004/Task025/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int powAB(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        res = res * a;
+    }
+    return res;
+}
+
+int pow = powAB(numberA, numberB);
+Console.WriteLine("Число " + numberA + " в степени " + numberB + " = " + pow);
