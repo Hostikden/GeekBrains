efa51a0ee34bde90cd050cc41c0ef1716b950e26
Hostik <Hostik.den@gmail.com>
2023-01-22 05:22:01 +0300

    fix Lesson006Task043

--- Homework/C#/Lesson006/Task043/Program.cs
+++ Homework/C#/Lesson006/Task043/Program.cs
@@ -6,13 +6,13 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 Console.Clear();
 System.Console.Write("Введите b1: ");
-double b1 = System.Convert.ToInt32(System.Console.ReadLine());
+int b1 = System.Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("Введите k1: ");
-double k1 = System.Convert.ToInt32(System.Console.ReadLine());
+int k1 = System.Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("Введите b2: ");
-double b2 = System.Convert.ToInt32(System.Console.ReadLine());
+int b2 = System.Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("Введите k2: ");
-double k2 = System.Convert.ToInt32(System.Console.ReadLine());
+int k2 = System.Convert.ToInt32(System.Console.ReadLine());
 
 ShowResult(b1, b2, k1, k2);
 
@@ -27,12 +27,19 @@ static void ShowResult(double b1, double b2, double k1, double k2)
         result = x + "" + y;
     }
 
+    if (Merge(b1, b2, k1, k2)) result = "линии совпадают";
+
     System.Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({result})");
 }
 
 static bool Parallel(double k1, double k2)
 {
     return k1 == k2;
+}
+
+static bool Merge(double b1, double b2, double k1, double k2)
+{
+    return k1 == k2 && b1 == b2 && k1 == b1;
 }
 
 static double SearchX(double b1, double b2, double k1, double k2)
