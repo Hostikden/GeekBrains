65b9020feda9fb014199874c0488a5d22745a2e4
Denis <Hostik.den@gmail.com>
2023-01-26 15:57:59 +0300

    add ArrayOrderBy

---
+++ C#/YuoTubeCS/ArrayOrderBy(Sort)/Program.cs
@@ -0,0 +1,12 @@
+﻿// Поиск уникальных элементов массива
+
+int[] myArray = { 10, 2, 1, 4, 5, 6, 2, 1, 90 };
+
+int[] result = myArray.OrderBy(i => i).ToArray();
+
+for (int i = 0; i < result.Length-1; i++)
+{
+    Console.Write($"{result[i]}\t");
+}
+
+System.Console.WriteLine();
