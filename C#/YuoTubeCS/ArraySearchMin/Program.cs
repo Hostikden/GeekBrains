86f922988fa870238220325b9a7334522e1f177e
Denis <Hostik.den@gmail.com>
2023-01-26 15:17:10 +0300

    add ArraySearchMin

---
+++ C#/YuoTubeCS/ArraySearchMin/Program.cs
@@ -0,0 +1,15 @@
+﻿// Поиск минимального значения в массиве
+
+int[] myArray = { 10, 2, 3, 4, 5, 6, 2, 1, 90 };
+
+int minValue = myArray[0];
+
+for (int i = 1; i < myArray.Length; i++)
+{
+    if (myArray[i] < minValue)
+    {
+        minValue = myArray[i];
+    }
+}
+
+Console.WriteLine(minValue);
