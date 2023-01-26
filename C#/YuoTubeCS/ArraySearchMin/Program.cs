fd6b2b5362936b8a0aa3ec3fb8fa2389f904c005
Denis <Hostik.den@gmail.com>
2023-01-26 15:54:24 +0300

    add ArrayDistinct

--- C#/YuoTubeCS/ArraySearchMin/Program.cs
+++ C#/YuoTubeCS/ArraySearchMin/Program.cs
@@ -2,14 +2,18 @@
 
 int[] myArray = { 10, 2, 3, 4, 5, 6, 2, 1, 90 };
 
-int minValue = myArray[0];
+// int minValue = myArray[0];
 
-for (int i = 1; i < myArray.Length; i++)
-{
-    if (myArray[i] < minValue)
-    {
-        minValue = myArray[i];
-    }
-}
+// for (int i = 1; i < myArray.Length; i++)
+// {
+//     if (myArray[i] < minValue)
+//     {
+//         minValue = myArray[i];
+//     }
+// }
+// Console.WriteLine(minValue);
 
-Console.WriteLine(minValue);
+// через метод
+
+Console.WriteLine(myArray.Min());
+
---
+++ C#/YuoTubeCS/ArraySum & Sum%2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Поиск суммы четных элементов массива
+
+int[] myArray = { 10, 2, 3, 4, 5, 6, 2, 1, 90 };
+
+Console.WriteLine(myArray.Where(i => i % 2 == 0).Sum());
