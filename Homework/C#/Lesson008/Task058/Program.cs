11f76d5ff85365964e7901cae0776a2375e179f4
Denis <Hostik.den@gmail.com>
2023-01-28 17:54:17 +0300

    add Lesson008Task058

--- Homework/C#/Lesson008/Task058/Program.cs
+++ Homework/C#/Lesson008/Task058/Program.cs
@@ -1,27 +1,28 @@
 ﻿//  Задача 58: Задайте две матрицы. Напишите программу, 
 //  которая будет находить произведение двух матриц.
 
-
 Console.Clear();
-
-
-int[,] firstMatrix = new int[2, 2]
+int[,] firstMatrix = new int[2, 3]
 {
-    {3,5},
-    {2,1}
+    {1,2,4},
+    {2,0,3}
 };
 
-int[,] secondMatrix = new int[2, 3]
+int[,] secondMatrix = new int[3, 2]
 {
-    {8,2,3},
-    {1,7,2}
+    {2,5},
+    {1,3},
+    {1,1}
 };
+
+int[,] tempMatrix = MultiplicationArray(firstMatrix, secondMatrix);
 
 Console.WriteLine($"##### MATRIX I ##### \n");
 PrintArray(firstMatrix);
 Console.WriteLine($"##### MATRIX II ##### \n");
 PrintArray(secondMatrix);
-
+Console.WriteLine($"##### RESULT MATRIX ##### \n");
+PrintArray(tempMatrix);
 
 void PrintArray(int[,] firstMatrix)
 {
@@ -35,4 +36,28 @@ void PrintArray(int[,] firstMatrix)
     }
 }
 
-
+int[,] MultiplicationArray(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] resultMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    if (firstMatrix.GetLength(1) != secondMatrix.GetLength(0))
+    {
+        Console.WriteLine($"\n Операция умножения двух матриц не выполнима. Число столбцов в первой матрице не равно числу строк во второй.");
+        return resultMatrix;
+    }
+    else
+    {
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < firstMatrix.GetLength(1); k++)
+                {
+                    sum += firstMatrix[i, k] * secondMatrix[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+    }
+    return resultMatrix;
+}
