7b02d236692e2269e15ee932676ed0f49060a8a0
Denis <Hostik.den@gmail.com>
2023-01-28 18:22:04 +0300

    add Lesson008Task054

--- Homework/C#/Lesson008/Task054/Program.cs
+++ Homework/C#/Lesson008/Task054/Program.cs
@@ -1,3 +1,58 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
 // которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
+Console.Clear();
+
+Console.Write("Введите количество строк массива: \t");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: \t");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 9);
+PrintArray(array);
+SortArray(array);
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($"{matr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void SortArray (int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    Console.WriteLine($"####### SORT MATRIX #######");
+}
