f68e493b8018159c34624814f1b7dd5dc852fd80
Denis <Hostik.den@gmail.com>
2023-01-30 17:30:46 +0300

    add Example167

---
+++ C#/GeekBrainsCS/Example167_SecretCount(Recursia)/Program.cs
@@ -0,0 +1,12 @@
+﻿// у рекурсии есть скрытый count
+// можно его задавать самому
+
+int number = 4;
+
+string Numbers(int number, int count = 0)
+{
+    if (count == number) return String.Empty;
+    return "count " + Numbers(number, count + 1);
+}
+
+Console.WriteLine(Numbers(number));
